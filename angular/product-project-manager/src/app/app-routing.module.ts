c40e2df53577dc878daba701019e076817048d40
chosenman <chosenman2008@gmail.com>
2017-07-20 22:55:37 -0700

    created all required components

--- angular/product-project-manager/src/app/app-routing.module.ts
+++ angular/product-project-manager/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductComponent } from './product/product.component';
+import { NewComponent } from './product/new/new.component';
+import { EditComponent } from './product/edit/edit.component';
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductComponent },
+  { path: 'products/new', component: NewComponent },
+  { path: 'products/edit/:id', component: EditComponent },
+  // <a [routerLink]="['/products','edit', edit.id]">Product Creation</a>
+  // {   path: '', children: [] }
 ];
 
 @NgModule({
--- angular/product-project-manager/src/app/app.module.ts
+++ angular/product-project-manager/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpService } from './http.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { NewComponent } from './product/new/new.component';
 import { EditComponent } from './product/edit/edit.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     NewComponent,
-    EditComponent
+    EditComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [],
